a5ae1e475d6443aad72ff3054d77f7cb23aec0d6
karina merulla <karina.merulla@gmail.com>
2018-11-01 17:26:53 -0300

    routing, controller e views for cocktails done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+ resources :cocktails, only: [:new, :create, :index, :show]
+ end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+json_file = open(url).read
+ingredients = JSON.parse(json_file)
+
+# puts ingredients["drinks"].first["strIngredient1"] --> para pegar o primeiro valor
+puts ingredients["drinks"]
+
+#vc descobre que eh uma hash com arrays de hashes
+
+# sendo uma array, ele vai poder usar o metodo each com um key e um value.
+# Mas eu quero o value, entao qndo ele passar, ele vai criar um metodo ingredients apenas com o valor da hash.
+ingredients['drinks'].each do |key, value|
+  Ingredient.create(name: value) # como no exemplo do kitt, mas eu queropegar o value do meu json
+end
+
+
